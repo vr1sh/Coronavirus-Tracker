834bbf5185d7c336dcafd14e4dff0d07f812b09b
Vrishank Mishra <42439148+vr1sh@users.noreply.github.com>
2020-09-15 15:23:50 +0530

    basic data fetching

--- src/main/java/com/vr1sh/CoronavirusTracker/services/CoronaVirusDataService.java
+++ src/main/java/com/vr1sh/CoronavirusTracker/services/CoronaVirusDataService.java
@@ -1,16 +1,24 @@
 package com.vr1sh.CoronavirusTracker.services;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.stereotype.Service;
+import javax.annotation.PostConstruct;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.sql.DatabaseMetaData;
 
+@Service
 public class CoronaVirusDataService {
 
-    private static String DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+    private static String DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
-    public void fetchVirusData() {
+    @PostConstruct
+    public void fetchVirusData() throws IOException, InterruptedException {
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(DATA_URL)).build();
@@ -18,6 +26,13 @@ public class CoronaVirusDataService {
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         System.out.println(response.body());
+
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(in);
+        for (CSVRecord record : records) {
+            String id = record.get("ID");
+            String customerNo = record.get("CustomerNo");
+            String name = record.get("Name");
+        }
 
     }
 
